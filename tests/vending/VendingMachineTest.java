67b874f58f48b7c3e206e93a52662b17f820dd7b
sbuDiction <nsbu68501@gmail.com>
2020-05-19 18:21:33 +0200

    changed test structure

--- tests/vending/VendingMachineTest.java
+++ tests/vending/VendingMachineTest.java
@@ -1,32 +1,23 @@
 package vending;
 
 import vending.product.Chocolates;
+import vending.product.SaltySnacks;
+import vending.product.SoftDrinks;
 import vending.product.Product;
 
 import org.junit.jupiter.api.Test;
-import vending.product.SaltySnacks;
-import vending.product.SoftDrinks;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VendingMachineTest {
 
     @Test
-    public void shouldShowTypeOfSnackChocolate() {
+    public void shouldShowDescriptionType() {
         Chocolates chocolate = new Chocolates();
-        assertEquals("This is a Chocolate",chocolate.description());
-    }
-
-    @Test
-        public void shouldShowTypeOfSnackSoftDrinks() {
-            SoftDrinks softDrink = new SoftDrinks();
-            assertEquals("This is a soft drink",softDrink.description());
-    }
-
-    @Test
-        public void shouldShowTypeOfSnackSaltySnacks() {
+        SoftDrinks softDrink = new SoftDrinks();
         SaltySnacks saltySnack = new SaltySnacks();
+        assertEquals("This is a soft drink",softDrink.description());
         assertEquals("This is a Salty snack",saltySnack.description());
+        assertEquals("This is a Chocolate",chocolate.description());
     }
 
 }
