b193660dbbf27620dd6de535343c0c0ed207ec18
sbuDiction <nsbu68501@gmail.com>
2020-05-19 14:52:30 +0200

    added the constructor for the product

---
+++ src/vending/Main.java
@@ -0,0 +1,5 @@
+package vending;
+
+public class Main {
+    public static void main(String[] args) {}
+}
--- src/vending/OverloadedVendingMachine.java
+++ src/vending/OverloadedVendingMachine.java
@@ -1,17 +1,26 @@
 package vending;
-
+// imports.
+import vending.product.Chocolates;
 import vending.product.Product;
+import vending.product.SaltySnacks;
+import vending.product.SoftDrinks;
+// class instance.
 
 public class OverloadedVendingMachine {
 
-    void buy(SoftDrink softdrink) {}
+    private int softDrinkQty1;
+    private int saltySnacksQty1;
+    private int chocolatesQty1;
 
-    void buy(SaltySnack saltySnack) {}
-
-    void buy(Chocolate chocolate) {}
-
+    void buy(SoftDrinks softdrink) {}
+    void buy(SaltySnacks saltySnack) {}
+    void buy(Chocolates chocolate) {}
     void buy(Product product) {}
 
-    OverloadedVendingMachine(int softDrinkQty, int saltySnacksQty, int chocolatesQty) {}
+    OverloadedVendingMachine(int softDrinkQty, int saltySnacksQty, int chocolatesQty) {
+        softDrinkQty1 = softDrinkQty;
+        saltySnacksQty1 = saltySnacksQty;
+        chocolatesQty1 = chocolatesQty;
+    }
 
 }
